c53c8770cc5e58feebb67ca78125d401daff0a2b
aficat <aficatrashid@gmail.com>
2024-05-16 17:50:11 +0800

    base storybook deployment

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y', //👈 The a11y addon goes here
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+};
+export default config;
